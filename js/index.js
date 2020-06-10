272497276eced1110245b803384546bd85b00475
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2020-06-10 20:41:51 +0700

    avoid global variable

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 // JavaScript Document
 "use strict";
 $(document).ready(function() {
-    var x = "x"
-    var o = "o"
-    var count = 0;
-    var o_win = 0;
-    var x_win = 0;
+    let x = "x"
+    let o = "o"
+    let count = 0;
+    let o_win = 0;
+    let x_win = 0;
     $('#game li').click(function(){
     
       if ($("#one").hasClass('o') && $("#two").hasClass('o') && $("#three").hasClass('o') || $("#four").hasClass('o') && $("#five").hasClass('o') && $("#six").hasClass('o') || $("#seven").hasClass('o') && $("#eight").hasClass('o') && $("#nine").hasClass('o') || $("#one").hasClass('o') && $("#four").hasClass('o') && $("#seven").hasClass('o') || $("#two").hasClass('o') && $("#five").hasClass('o') && $("#eight").hasClass('o') || $("#three").hasClass('o') && $("#six").hasClass('o') && $("#nine").hasClass('o') || $("#one").hasClass('o') && $("#five").hasClass('o') && $("#nine").hasClass('o') || $("#three").hasClass('o') && $("#five").hasClass('o') && $("#seven").hasClass('o'))
